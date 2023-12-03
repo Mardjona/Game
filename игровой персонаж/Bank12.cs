e48471711de1df6b9f12a4f3cc35bdff5f154825
Mardjona <114858484+Mardjona@users.noreply.github.com>
2023-12-04 00:29:04 +0300

    Add files via upload

---
+++ игровой персонаж/Bank12.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+class GameCharacter
+{
+    // Свойства класса
+     string Name { get; set; }
+     int MaxHealth { get; set; }
+     int CurrentHealth { get; set; }
+     bool IsAlly { get; set; }
+     int Coordinate { get; set; }
+     int Wins { get; set; }
+
+    // Методы класса
+    public void InputInformation()
+    {
+        Console.WriteLine("Введите информацию о персонаже:");
+        Console.Write("Имя: ");
+        Name = Console.ReadLine();
+        Console.Write("Максимальное здоровье: ");
+        MaxHealth = int.Parse(Console.ReadLine());
+        CurrentHealth = MaxHealth;
+        Console.Write("Принадлежность к лагерю (true - союзник, false - враг): ");
+        IsAlly = bool.Parse(Console.ReadLine());
+        Console.Write("Расположение в координате: ");
+        Coordinate = int.Parse(Console.ReadLine());
+        Wins = 0;
+    }
+
+    public void DisplayInformation()
+    {
+        Console.WriteLine("Информация о персонаже:");
+        Console.WriteLine($"Имя: {Name}");
+        Console.WriteLine($"Максимальное здоровье: {MaxHealth}");
+        Console.WriteLine($"Текущее здоровье: {CurrentHealth}");
+        Console.WriteLine($"Принадлежность к лагерю: {(IsAlly ? "Союзник" : "Враг")}");
+        Console.WriteLine($"Расположение в координате: {Coordinate}");
+        Console.WriteLine($"Количество побед: {Wins}");
+    }
+
+    public bool IsInSameCoordinate(GameCharacter otherCharacter)
+    {
+        return Coordinate == otherCharacter.Coordinate;
+    }
+
+    public void Attack(GameCharacter enemy)
+    {
+        Console.WriteLine($"Персонаж {Name} атакует персонажа {enemy.Name}");
+        enemy.CurrentHealth -= 10;
+        if (enemy.CurrentHealth <= 0)
+        {
+            enemy.CurrentHealth = 0;
+            enemy.Wins++;
+        }
+        Console.WriteLine($"Персонаж {enemy.Name} получает урон, текущее здоровье: {enemy.CurrentHealth}");
+    }
+
+    public void RestoreHealth()
+    {
+        if (Wins >= 5)
+        {
+            Console.WriteLine($"Персонаж {Name} использовал полное лечение.");
+            CurrentHealth = MaxHealth;
+        }
+        else
+        {
+            Console.WriteLine("Недостаточно побед для использования полного лечения.");
+        }
+    }
+
+    public void Heal(GameCharacter teammate)
+    {
+        if (IsAlly && IsInSameCoordinate(teammate))
+        {
+            Console.WriteLine($"Персонаж {Name} лечит персонажа {teammate.Name}");
+            int healAmount = Math.Min(CurrentHealth, teammate.MaxHealth - teammate.CurrentHealth);
+            CurrentHealth -= healAmount;
+            teammate.CurrentHealth += healAmount;
+            Console.WriteLine($"Персонаж {teammate.Name} восстановил здоровье на {healAmount}, текущее здоровье: {teammate.CurrentHealth}");
+        }
+        else
+        {
+            Console.WriteLine("Невозможно применить лечение к данному персонажу.");
+        }
+    }
+}
+
---
+++ игровой персонаж/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество игроков: ");
+        int playerCount = int.Parse(Console.ReadLine());
+
+        if (playerCount >= 2)
+        {
+            GameCharacter[] players = new GameCharacter[playerCount];
+
+            for (int i = 0; i < playerCount; i++)
+            {
+                players[i] = new GameCharacter();
+                players[i].InputInformation();
+            }
+
+            Console.WriteLine("Игра началась!");
+
+            // Пример использования методов класса
+            players[0].Attack(players[1]);
+
+            players[0].RestoreHealth();
+
+            players[0].Heal(players[1]);
+
+            players[0].DisplayInformation();
+            players[1].DisplayInformation();
+        }
+        else
+        {
+            Console.WriteLine("Недостаточно игроков для начала игры.");
+        }
+    }
+}
